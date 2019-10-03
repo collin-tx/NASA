b90158ab4eff77acddbea3c57d4ce959b6964fa0
Collin Smith <cmsmith.tx@gmail.com>
2019-10-03 16:16:45 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Apod from './Components/Apod';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/apod" component={Apod} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
