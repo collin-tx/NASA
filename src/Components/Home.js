3d16cce9f01fa44b747e247abefeb0efa07f1ee8
Collin Smith <cmsmith.tx@gmail.com>
2019-10-03 16:08:42 -0500

    adding home component/styles

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div id="home">
+            <div id="home-text">
+                <h1>NASA</h1>
+                <p>Explore.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
